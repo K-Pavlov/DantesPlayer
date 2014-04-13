01ed4ff79bc7a3416188759acb5b96f1fb0f5d47
Alexander <alexchowe@gmail.com>
2014-04-13 01:32:23 -0700

    Create CheckException.cs

---
+++ DantesPlayer/MainScreen/CheckException.cs
@@ -0,0 +1,20 @@
+namespace MainScreen
+ {
+     #region Namespaces
+     using System;
+     using System.Diagnostics;
+     #endregion
+     public static class CheckException
+     {
+         /// <summary>
+         /// Returns true if not null
+         /// </summary>
+         /// <param name="objectToCheck"></param>
+         /// <returns></returns>
+ public static bool CheckNull(object objectToCheck)
+         {
+             bool isNotNull = objectToCheck != null;
+             return isNotNull;
+         }   
+     }
+ }
